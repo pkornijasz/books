397c43591b5de560365b01b10aca03e5af7e513e
pkornijasz <pk@nkk.pl>
2021-06-12 13:13:06 +0200

    M8L11

--- src/test/java/pl/kornijasz/books/catalog/web/CatalogControllerApiTest.java
+++ src/test/java/pl/kornijasz/books/catalog/web/CatalogControllerApiTest.java
@@ -23,28 +23,28 @@ import static org.springframework.boot.test.context.SpringBootTest.*;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CatalogControllerApiTest {
 
-//    @LocalServerPort
-//    private int port;
-//
-//    @MockBean
-//    CatalogUseCase catalogUseCase;
-//
-//    @Autowired
-//    TestRestTemplate restTemplate;
-//
-//    @Test
-//    void getAllBooks() {
-//        // given
-//        Book effective = new Book("Effective Java", 2005, new BigDecimal("99.90"), 50L);
-//        Book concurrency = new Book("Java Concurrency in Practice", 2006, new BigDecimal("99.90"), 50L);
-//        Mockito.when(catalogUseCase.findAll()).thenReturn(List.of(effective, concurrency));
-//        ParameterizedTypeReference<List<Book>> type = new ParameterizedTypeReference<>() {
-//        };
-//        // when
-//        String url = "http://localhost:" + port + "/catalog";
-//        RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:" + port + "/catalog")).build();
-//        ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
-//        // then
-//        assertEquals(2, response.getBody().size());
-//    }
+    @LocalServerPort
+    private int port;
+
+    @MockBean
+    CatalogUseCase catalogUseCase;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void getAllBooks() {
+        // given
+        Book effective = new Book("Effective Java", 2005, new BigDecimal("99.90"), 50L);
+        Book concurrency = new Book("Java Concurrency in Practice", 2006, new BigDecimal("99.90"), 50L);
+        Mockito.when(catalogUseCase.findAll()).thenReturn(List.of(effective, concurrency));
+        ParameterizedTypeReference<List<Book>> type = new ParameterizedTypeReference<>() {
+        };
+        // when
+        String url = "http://localhost:" + port + "/catalog";
+        RequestEntity<Void> request = RequestEntity.get(URI.create(url)).build();
+        ResponseEntity<List<Book>> response = restTemplate.exchange(request, type);
+        // then
+        assertEquals(2, response.getBody().size());
+    }
 }
